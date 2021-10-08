68d861b2d0ea86311cf41454568170993bbc75d1
luidor <albertosantos275@gmail.com>
2021-10-08 16:20:40 -0400

    chore(FileSystem): add file system setitng

--- a_bank/admin.py
+++ a_bank/admin.py
@@ -1,17 +1,24 @@
 
 
 from django.contrib import admin
-from a_bank.models import Bank, File, Batch
+from a_bank.models import Bank, File, Batch, FileTypes
 
 class BankAdmin(admin.ModelAdmin):
     list_display= ('name',)
 
 class FileAdmin(admin.ModelAdmin):
+    list_display= ('file_name',)
+
+class FileTypesAdmin(admin.ModelAdmin):
     list_display= ('name',)
 
 class BatchAdmin(admin.ModelAdmin):
-    list_display= ('status',)
+    list_display= ('bank_id','status','create_on')
+
+
+
 
 admin.site.register(Bank,BankAdmin)
 admin.site.register(File,FileAdmin)
+admin.site.register(FileTypes,FileTypesAdmin)
 admin.site.register(Batch,BatchAdmin)
--- a_bank/models.py
+++ a_bank/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import datetime
-
+from django.utils.translation import gettext_lazy as _
+import uuid
+import os
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -15,23 +17,47 @@ class Bank(models.Model):
 
 
 class Batch(models.Model):
-    status = models.CharField(max_length=100, blank=False, unique=True)
+    class BatchStatus(models.TextChoices):
+        PENDING = 'pending', _('Pending')
+        PROCESSING = 'processing', _('Processing')
+        DONE = 'Done', _('Done')
     bank_id = models.ForeignKey(Bank,related_name='batch_bank',on_delete=models.CASCADE,unique=False,blank=False,null=False)
+    status = models.CharField(max_length=30, choices= BatchStatus.choices,default=BatchStatus.PENDING)
     store_path = models.CharField(max_length=200, blank=True)
     processed_time = models.DateTimeField(default=datetime.now, blank=True)
     create_on = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
-        return self.status + "--" + self.bank_id
+        return self.bank_id.name+ '--' +self.status + "--" + str(self.bank_id.id)
     class Meta:
         verbose_name = '2-Batch'
 
 
-class File(models.Model):
-    batch_id = models.ForeignKey(Batch,related_name='file_batch',on_delete=models.CASCADE,unique=False,blank=False,null=False)
-    file_type = models.CharField(max_length=100, blank=False, unique=True)
+class FileTypes(models.Model):    
     name = models.CharField(max_length=100, blank=False, unique=True)
+    code = models.CharField(max_length=20, blank=True, unique=False)
     create_on = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
-        return self.name
+        return self.name + ' -- ' +  self.code
     class Meta:
-        verbose_name = '3-File'
+        verbose_name = '3-File Type'
+
+
+def get_file_path(instance, filename):
+    print(instance, filename)
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join(instance.batch_id.bank_id.name + '/' +instance.file_type_id.name, filename)
+
+class File(models.Model):
+    batch_id = models.ForeignKey(Batch,related_name='file_batch',on_delete=models.CASCADE,unique=False,blank=False,null=False)
+    file_type_id = models.ForeignKey(FileTypes,related_name='file_type_files',on_delete=models.CASCADE,unique=False,blank=True,null=True)
+    file_url = models.FileField('Document', upload_to = get_file_path, blank=True,null=True)
+    file_name  = models.CharField(max_length=100, blank=True, unique=False, )
+    create_on = models.DateTimeField(default=datetime.now, blank=True, )
+    def __str__(self):
+        return self.file_name
+    class Meta:
+        verbose_name = '4-File'
+
+
+
